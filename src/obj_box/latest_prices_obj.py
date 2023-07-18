9ba3eb7651d3718bddd1f1541bf4b1b29afe2316
Franklyn Aquino <f.aquino@lukka.global>
2023-07-18 17:04:32 -0400

    Added Latency, edit to obj, less logging

--- src/obj_box/latest_prices_obj.py
+++ src/obj_box/latest_prices_obj.py
@@ -1,10 +1,16 @@
 class LatestPricesObj:
-    def __init__(self, pair_code,ts,current_ts,prices=None,latency=None):
+    def __init__(self, pair_code,ts,current_ts,prices=None,latency=None,version=None,source=None):
         self.pair_code = pair_code
         self.ts = ts
         self.curren_ts = current_ts
         self.prices = prices
         self.latency = latency
+        self.version = version
+        self.source = source
+        
         
     def __str__(self):
-        return f'pair_code: {self.pair_code}, ts: {self.ts}, current_ts: {self.curren_ts}, prices: {self.prices}'
+        if self.version is None and self.source is None:
+            return f'pair_code: {self.pair_code}, ts: {self.ts}, current_ts: {self.curren_ts}, prices: {self.prices}, latency: {str(self.latency)[:-4]}\'s'
+        else:
+            return  f'pair_code: {self.pair_code}, ts: {self.ts}, current_ts: {self.curren_ts}, prices: {self.prices}, version: {self.version}, source: {self.source}, latency: {str(self.latency)[:-4]}\'s'
