97bb2f312e571fcb4ef8d6e1db8a865de82a3000
Franklyn Aquino <f.aquino@lukka.global>
2023-07-18 17:04:32 -0400

    Added Latency, edit to obj, less logging

--- src/my_utils/myUtils.py
+++ src/my_utils/myUtils.py
@@ -10,11 +10,11 @@ def get_logger(name):
     logger.setLevel(logging.INFO)
     # create a logging format
     # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    formatter = logging.Formatter('%(message)s')
     # add the console handler
     consoleHandler = logging.StreamHandler()
     consoleHandler.setFormatter(formatter)
-    consoleHandler.setFormatter(formatter)
+    # consoleHandler.setFormatter(formatter)
     # add handler
     logger.addHandler(consoleHandler)
     return logger
--- src/run.py
+++ src/run.py
@@ -77,18 +77,32 @@ else:
     
     if args.version is not None and args.source is not None:
         list_of_latest_prices:List[LatestPricesObj] = []
+        list_of_missing_latest_pairs = []
         args_url = f'{portal}/{args.version}/pricing/sources/{args.source}/prices?pairCodes={pair_codes}'
         date_now_utc:datetime = datetime.utcnow()
         logger.info(f'Your current Region is: {"West/DR" if args.region=="w" else "East/PROD"}')
         # logger.info(f'Your current time is: {date_now_utc}')
-        logger.info(f'TESTING Your current time is: ', date_now_utc.strftime('%Y/%m/%d %H:%M'))
+        logger.info("Your current time is: " + date_now_utc.strftime('%Y/%m/%d %H:%M:%S'))
         try:
-            response = get(url=args_url, headers=headers).json()
-            for item in response:
+            ###
+            ### Latency for command with args is calculated here
+            ###
+            latency_start = datetime.utcnow()
+            response = get(url=args_url, headers=headers)
+            latency_end = datetime.utcnow() - latency_start
+            latency_end = latency_end.total_seconds()
+            
+            #TODO: This if statemenet below doesn't work! 
+            if len(response.text) <= 0 :
+                list_of_missing_latest_pairs.append(pair_codes)
+            
+            logger.info(f'your response -> {response.text}')
+            for item in response.json():
                 current_latest_obj = LatestPricesObj(pair_code=item['pairCode'],
                                                     ts=item['ts'],
                                                     current_ts=date_now_utc,
-                                                    prices=item['price'],)
+                                                    prices=item['price'],
+                                                    latency=latency_end,)
                 logger.info(f'{current_latest_obj.__str__()}')
                 list_of_latest_prices.append(current_latest_obj)
         except exceptions.JSONDecodeError as e:
@@ -99,15 +113,19 @@ else:
         five_min_lag = timedelta(minutes=5)
         three_min_lag = timedelta(minutes=3)
         one_min_lag = timedelta(minutes=1)
-        
+        prices_failure_counter = 0
+        list_of_specified_latencies = []
         if(args.source == '10500'):
             for latest_price in list_of_latest_prices:
                 latest_ts = datetime.strptime(latest_price.ts, '%Y-%m-%dT%H:%M:%SZ')
                 lag = latest_price.curren_ts - latest_ts
                 match = re.compile(r'[0-9]\.[0-9]{3}').search(latest_price.prices)
-                if lag <= one_min_lag and match:
+                if lag >= one_min_lag and match:
                     logger.info(f'V1(10500): {latest_price.pair_code} is within current time: difference: {lag} & prices include sub-pennies ({latest_price.prices})')
                 else:
+                    prices_failure_counter += 1
+                    latest_price.version_source = f'{args.version}({args.source}'
+                    list_of_specified_latencies.append(latest_price)
                     logger.info(f'V1(10500): {latest_price.pair_code} is NOT within current time, difference: {lag} or prices doesn\'t include sub-pennies ({latest_price.prices})')
         else:
             for latest_price in list_of_latest_prices:
@@ -117,8 +135,21 @@ else:
                 if lag >= three_min_lag and lag < five_min_lag:
                     logger.info(f'{args.version}({args.source}): {latest_price.pair_code} is within 3-4 minutes of current time, difference: {lag}')
                 else:
+                    prices_failure_counter += 1
+                    latest_price.version_source = f'{args.version}({args.source})'
+                    list_of_specified_latencies.append(latest_price)
                     logger.info(f'{args.version}({args.source}): {latest_price.pair_code} is NOT within 3-4 minutes of current time, difference: {lag}')
-                    
+        
+        if (prices_failure_counter == 0):
+            logger.info(f'\nALL Tests Passed')
+        else:
+            logger.info(f'\nPairs that failed:')
+            for latest_price in list_of_specified_latencies:
+                logger.info(f'{latest_price.__str__()}')
+            
+            if list_of_missing_latest_pairs is not None:
+                logger.info(f'\nPairs that are missing: {list_of_missing_latest_pairs}')
+                
     elif args.run_all:
         pricingv1_url ="/v1/pricing/sources/2000/prices"
         pricingv1_10500_url ="/v1/pricing/sources/10500/prices"
@@ -136,9 +167,17 @@ else:
             full_url = f'{portal}{url}?pairCodes={pair_codes}'
             date_now_utc:datetime = datetime.utcnow()
             # logger.info(f'Your current time is: {date_now_utc}')
-            print("TESTING Your current time is: " + date_now_utc.strftime('%Y/%m/%d %H:%M:%S'))
+            logger.info("Your current time is: " + date_now_utc.strftime('%Y/%m/%d %H:%M:%S'))
             try:
-                response = get(url=full_url, headers=headers).json()
+                ###
+                ### Latency for run_all is calculated here
+                ###
+                latency_start = datetime.utcnow()
+                response = get(url=full_url, headers=headers)
+                latency_end = datetime.utcnow() - latency_start
+                latency_end = latency_end.total_seconds()
+                
+                response = response.json()
             except exceptions.JSONDecodeError as e:
                 logger.info(f'Please use valid bearer token')
                 break
@@ -147,7 +186,11 @@ else:
                 break
             
             for item in response:
-                current_latest_obj = LatestPricesObj(pair_code=item['pairCode'],ts=item['ts'],current_ts=date_now_utc, prices=item['price'])
+                current_latest_obj = LatestPricesObj(pair_code=item['pairCode'],
+                                                    ts=item['ts'],
+                                                    current_ts=date_now_utc,
+                                                    prices=item['price'],
+                                                    latency=latency_end)
                 logger.info(f'{current_latest_obj.__str__()}')
                 if url == pricingv1_url:
                     list_of_latest_pricesv1.append(current_latest_obj)
@@ -168,6 +211,8 @@ else:
                 logger.info(f'V1(2000): {latest_price.pair_code} is within 3-4 minutes of current time, difference: {lag}')
             else:
                 v1_prices_failure_counter += 1
+                latest_price.version = 'V1'
+                latest_price.source = '2000'
                 list_of_affected_latencies.append(latest_price)
                 logger.info(f'V1(2000): {latest_price.pair_code} is NOT within 3-4 minutes of current time, difference: {lag}')
         
@@ -179,6 +224,8 @@ else:
                 logger.info(f'V1(10500): {latest_price.pair_code} is within current time: difference: {lag} & prices include sub-pennies ({latest_price.prices})')
             else:
                 v1s10500_prices_failure_counter += 1
+                latest_price.version = 'V1'
+                latest_price.source = '10500'
                 list_of_affected_latencies.append(latest_price)
                 logger.info(f'V1(10500): {latest_price.pair_code} is NOT within current time, difference: {lag} or prices doesn\'t include sub-pennies ({latest_price.prices})')
                 
@@ -190,13 +237,17 @@ else:
                 logger.info(f'V3(2000): {latest_price.pair_code} is within 3-4 minutes of current time, difference: {lag}')
             else:
                 v3_prices_failure_counter += 1
+                latest_price.version = 'V3'
+                latest_price.source = '2000'
                 list_of_affected_latencies.append(latest_price)
                 logger.info(f'V3(2000): {latest_price.pair_code} is NOT within 3-4 minutes of current time, difference: {lag}')
             
         if (v1_prices_failure_counter == 0) and (v1s10500_prices_failure_counter == 0) and (v3_prices_failure_counter == 0):
-            logger.info(f'ALL Tests Passed')
+            logger.info(f'\nALL Tests Passed')
         else:
-            logger.info(f'Pairs that failed: {list_of_affected_latencies.__str__()}')
+            logger.info(f'\nPairs that failed:')
+            for latest_price in list_of_affected_latencies:
+                logger.info(f'{latest_price.__str__()}')
     else:
         logger.info('Checking other arguments')
 
