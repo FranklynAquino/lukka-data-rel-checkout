212f637acb583cffb8f7b1f79a801e8da956e67b
Franklyn Aquino <f.aquino@lukka.global>
2023-07-20 13:13:43 -0400

    Added strict exceptions, removed log

--- src/factory/latest_prices.py
+++ src/factory/latest_prices.py
@@ -1,6 +1,6 @@
 from requests import (get, exceptions)
 from json import (decoder)
-from dataclasses import (dataclass, field, fields, InitVar,)
+from dataclasses import (dataclass, field, InitVar,)
 from typing import List
 from .obj_box.latest_prices_obj import LatestPricesObj
 from datetime import datetime
@@ -30,21 +30,21 @@ class LatestPrices:
                 self.portal = "https://data-pricing-east.lukka.tech"
             case 'w':
                 self.portal = "https://data-pricing-west.lukka.tech"
-
+                
         if pair_codes is not None:
             self.pair_codes = ','.join(pair_codes)
         self.list_of_pair_codes = self.pair_codes.split(',')
-
         self.list_of_missing_latest_pairs = []
+        self.list_of_latest_prices = []
 
     def parse_latest_prices(self) -> List[LatestPricesObj]:
         args_url = f'{self.portal}/{self.version}/pricing/sources/{self.source}/prices?pairCodes={self.pair_codes}'
         date_now_utc: datetime = datetime.utcnow()
-        self.logger.info(
-            f'Your current Region is: {"West/DR" if self.region=="w" else "East/PROD"}')
-        self.logger.info("Your current time is: " +
-                         date_now_utc.strftime('%Y/%m/%d %H:%M:%S'))
-        list_of_latest_prices = []
+        print(
+            #TODO: Add region to output for PROD
+            f'Your current Region is: {"West" if self.region=="w" else "East"}')
+        print("Your current time is: " +
+                        date_now_utc.strftime('%Y/%m/%d %H:%M:%S'))
         try:
             ###
             # Latency for command with args is calculated here
@@ -65,13 +65,17 @@ class LatestPrices:
                                                      latency=request_time,
                                                      version=self.version,
                                                      source=self.source)
-                self.logger.info(f'{current_latest_obj.__repr__()}')
-                list_of_latest_prices.append(current_latest_obj)
-            return list_of_latest_prices
+                print(f'{current_latest_obj.__repr__()}')
+                self.list_of_latest_prices.append(current_latest_obj)
+            return self.list_of_latest_prices
+        
         except exceptions.JSONDecodeError as e:
-            self.logger.info(f'Please use valid bearer token')
+            self.logger.error(f'Please use valid bearer token')
         except decoder.JSONDecodeError as e:
-            self.logger.info(f'Please use valid bearer token')
+            self.logger.error(f'Please use valid bearer token')
 
     def get_list_of_missing_latest_pairs(self) -> List[str]:
         return self.list_of_missing_latest_pairs
+    
+    def get_list_of_latest_prices(self) -> List[LatestPricesObj]:
+        return self.list_of_latest_prices
--- src/lag_check.py
+++ src/lag_check.py
@@ -6,7 +6,7 @@ from my_utils.myUtils import get_logger
 from factory.obj_box.latest_prices_obj import LatestPricesObj
 
 class LagChecker:
-    def __init__(self, list_of_latest_prices: List[LatestPricesObj], source: str = None, version: str = None):
+    def __init__(self, list_of_latest_prices: List[LatestPricesObj], source: str = None, version: str = None,logger=None):
         self.list_of_latest_prices = list_of_latest_prices
         self.source = source
         self.version = version
@@ -15,7 +15,6 @@ class LagChecker:
         self.three_min_lag = timedelta(minutes=3)
         self.one_min_lag = timedelta(minutes=1)
         self.lag_failure_counter = 0
-        self.logger = get_logger("Lag Calculator")
 
     def calculate_latencies(self) -> List[LatestPricesObj]:
         list_of_lagged_ts = []
@@ -28,29 +27,29 @@ class LagChecker:
                     match = re.compile(
                         r'[0-9]\.[0-9]{3}').search(latest_price.prices)
                     if lag <= self.one_min_lag and match:
-                        self.logger.info(
+                        print(
                             f'{self.version}({self.source}): {latest_price.pair_code} is within current time: difference: {lag} & prices include sub-pennies ({latest_price.prices})')
                     else:
                         self.lag_failure_counter += 1
                         latest_price.version = f'{self.version}'
                         latest_price.source = f'{self.source}'
                         list_of_lagged_ts.append(latest_price)
-                        self.logger.info(
+                        print(
                             f'{self.version}({self.source}):{latest_price.pair_code} is NOT within current time, difference: {lag} or prices doesn\'t include sub-pennies ({latest_price.prices})')
-                    return list_of_lagged_ts
+                return list_of_lagged_ts
             case _:
                 for latest_price in self.list_of_latest_prices:
                     latest_ts = datetime.strptime(
                         latest_price.ts, '%Y-%m-%dT%H:%M:%SZ')
                     lag = latest_price.current_ts - latest_ts
                     if lag >= self.three_min_lag and lag < self.five_min_lag:
-                        self.logger.info(
+                        print(
                             f'{self.version}({self.source}): {latest_price.pair_code} is within 3-4 minutes of current time, difference: {lag}')
                     else:
                         self.lag_failure_counter += 1
                         latest_price.version = f'{self.version}'
                         latest_price.source = f'{self.source}'
                         list_of_lagged_ts.append(latest_price)
-                        self.logger.info(
+                        print(
                             f'{self.version}({self.source}): {latest_price.pair_code} is NOT within 3-4 minutes of current time, difference: {lag}')
                 return list_of_lagged_ts
